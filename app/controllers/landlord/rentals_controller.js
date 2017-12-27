ee034270e33395d37ab700e0b35a316832c7882c
Rene Garcia <rene.garciah90@gmail.com>
2017-12-27 13:23:12 -0600

    Adding rentals endpoint and modifying table to have safe delete

---
+++ app/controllers/landlord/rentals_controller.js
@@ -0,0 +1,88 @@
+const Rental = require('../models').rental;
+const express = require('express')
+const router = express.Router()
+const route = '/rentals'
+
+router.use(function (req, res, next) {
+  if (req.params.id) {
+    Rental
+      .where({ id: req.params.id })
+      .then(function (rental) {
+        req.mergedParams.currentRental = rental
+        next();
+      });
+  } else {
+    next();
+  }
+
+});
+
+function index(req, res) {
+  const page = req.mergedParams.page || 1;
+  const pageSize = 10;
+  const landlordId = req.mergedParams.landlordId;
+  Rental
+    .where({ landlord_id: landlordId })
+    .fetchPage({ page: page, pageSize: pageSize })
+    .then(function (rentals) {
+      console.log(rentals);
+      res.send(rentals.toJSON())
+    })
+    .catch(function (error) {
+      console.log('Error on GET /properties ===> ', error);
+      res.status(400).json({ error_message: 'Error while fetching properties' })
+    })
+}
+
+function create(req, res) {
+  const rentalParams = request.mergedParams.rentalParams;
+  Rental.forge(rentalParams)
+    .save()
+    .then(function (rental) {
+      res.json(rental.toJSON())
+    })
+    .catch(function (error) {
+      console.log('Error on GET /properties ===> ', error);
+      res.status(400).json({ error_message: 'Error while Saving new property', error: error })
+    })
+}
+
+function show(req, res) {
+  const rental = req.mergedParams.currentRental
+  res.send(rental.toJSON())
+}
+
+function update(req, res) {
+  const requestedRental = req.mergedParams.currentRental
+  const rentalParams = req.mergedParams.rentalParams
+  requestedRental.set(rentalParams)
+    .save()
+    .then(function (rental) {
+      res.json(rental.toJSON())
+    })
+    .catch(function (error) {
+      console.log('Error on GET /properties ===> ', error);
+      res.status(400).json({ error_message: 'Error while fetching properties', error: error })
+    });
+}
+
+function destroy(req, res) {
+  const requestedRental = req.mergedParams.currentRental
+  requestedRental.set({ finished: true })
+    .save()
+    .then(function (rental) {
+      res.json(rental.toJSON())
+    })
+}
+
+
+router.get('/', index);// GET /properties
+router.post('/', create);// POST /properties
+router.get('/:id', show)// GET /properties/:id
+router.put(':/id', update)// PUT /properties/:id
+router.delete('/:id/disable', destroy);// DELETE /properties/:id/disable
+
+module.exports = {
+  router: router,
+  route: route
+}
--- migrations/20171114103455_create_table_rentals.js
+++ migrations/20171114103455_create_table_rentals.js
@@ -3,12 +3,15 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('rentals', function (table) {
     table.increments('id').primary();
     table.integer('tenant_id');
+    table.integer('landlord_id');
+    table.boolean('vacant').defaultTo(true);;
+    table.boolean('required_gender');
+    table.string('contract_document_url');
+    table.boolean('finished').defaultTo(false);
+    table.foreign('landlord_id').references('landlords.id');
     table.integer('contract_type_id').references('contract_types.id');
     table.integer('property_id').references('properties.id')
     table.foreign('tenant_id').references('users.id');
-    table.boolean('vacant')
-    table.boolean('required_gender')
-    table.string('contract_document_url');
     table.timestamps();
   })
 };
